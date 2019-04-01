9f9e6c89f4fa18525372563234e62b884e25fef9
Steven-Blair <stevenblair@ksu.edu>
2019-04-01 13:19:38 -0500

    notes for lab

---
+++ Ksu.Cis300.PriorityQueueLibrary/LeftistTree.cs
@@ -0,0 +1,76 @@
+﻿/* BinaryTreeNode.cs
+ * Author: Rod Howell */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KansasStateUniversity.TreeViewer2;
+
+namespace Ksu.Cis300.PriorityQueueLibrary
+{
+    /// <summary>
+    /// An immutable generic binary tree node that can draw itself.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements stored in the tree.</typeparam>
+    public partial class LeftistTree<T> : ITree
+    {
+
+        private int _nullPathLength;
+
+        /// <summary>
+        /// Gets the data stored in this node.
+        /// </summary>
+        public T Data { get; }
+
+        /// <summary>
+        /// Gets this node's left child.
+        /// </summary>
+        public LeftistTree<T> LeftChild { get; }
+
+        /// <summary>
+        /// Gets this node's right child.
+        /// </summary>
+        public LeftistTree<T> RightChild { get; }
+
+        /// <summary>
+        /// Constructs a BinaryTreeNode with the given data, left child, and right child.
+        /// </summary>
+        /// <param name="data">The data stored in the node.</param>
+        /// <param name="left">The left child.</param>
+        /// <param name="right">The right child.</param>
+        /// Modify the constructor so that it initializes:
+        ///RightChild to the given child with smaller null path length(use the above method);
+        ///LeftChild to the other given child; and
+        ///the null path length field to 1 more than the null path length of the right child.
+        public LeftistTree(T data, LeftistTree<T> left, LeftistTree<T> right)
+        {
+            Data = data;
+            if (NullPathLength(right) < NullPathLength(left))
+            {
+                RightChild = right;
+                LeftChild = left;
+            }
+            else
+            {
+                // the left child is smaller
+                RightChild = left;
+                LeftChild = right;
+            }
+            // make sure students use the correct right child
+            _nullPathLength = 1 + NullPathLength(RightChild);
+        }
+
+        public static int NullPathLength(LeftistTree<T> t)
+        {
+            if (t == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return t._nullPathLength;
+            }
+        }
+    }
+}
